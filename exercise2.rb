ae2e6326955915d5c970d1d4f9400377f6a203b8
Julia Hopkins <juliahopkins@Julias-MacBook-Pro.local>
2013-10-22 13:24:43 -0400

    Second exercise

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+
+#Calculating 20% tip for $55 meal
+meal = 55
+tip_pct = 0.2
+puts tip = meal*tip_pct
+
+# add string and integer, converting integer to string
+puts "string" + 5.to_s
+
+# add two numbers using string interpolation
+x = 45628
+y = 7839
+
+puts "adding x + y: #{x + y}"
+
+#true or false
+puts (true && false) || (false && true) || !(false && false)
